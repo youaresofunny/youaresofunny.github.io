54a5db6fba3e1804e289dacf7b51847d3f9d7156
youaresofunny <anlopan@gmail.com>
2020-03-14 14:06:18 +0500

    Add test script

---
+++ embed.js
@@ -0,0 +1,71 @@
+(function () {
+  'use strict';
+
+  const env = {"BASE_URL":"https://dev.randomcoffee.us"};
+
+  function isForumNavEvent(e) {
+      return e.type === 'forum-navigation';
+  }
+  function isForumTitleEvent(e) {
+      return e.type === 'forum-title';
+  }
+
+  (() => {
+      const DATA_ATTR = 'data-forum-id';
+      const thisScriptTag = document.querySelector(`script[${DATA_ATTR}]`);
+      if (!thisScriptTag)
+          return; // should not be possible
+      const communityID = thisScriptTag.getAttribute(DATA_ATTR);
+      if (!communityID) {
+          console.error(`${DATA_ATTR} must be defined`);
+          return;
+      }
+      const baseURL = env.BASE_URL;
+      const prefixRgx = new RegExp(`^/${communityID}`);
+      const forumURL = new URL(baseURL);
+      const { pathname, search } = window.location;
+      forumURL.pathname = `/${communityID}${pathname}`;
+      forumURL.search = search;
+      const iframe = document.createElement('iframe');
+      iframe.src = forumURL.href;
+      iframe.style.border = 'none';
+      iframe.style.width = '100%';
+      iframe.style.height = '100%';
+      document.body.insertBefore(iframe, thisScriptTag);
+      const sendMessage = (msg) => {
+          const w = iframe.contentWindow;
+          if (!w) {
+              console.error('no child window');
+              return;
+          }
+          w.postMessage(msg, baseURL);
+      };
+      window.addEventListener('popstate', () => sendMessage({
+          type: 'forum-popstate',
+      }));
+      window.addEventListener('message', event => {
+          if (event.origin !== forumURL.origin) {
+              return; // some other window sent this event
+          }
+          const payload = event.data;
+          if (isForumNavEvent(payload)) {
+              const location = payload.pathname.replace(prefixRgx, '') + payload.search;
+              history.pushState(null, '', location);
+              return;
+          }
+          if (isForumTitleEvent(payload)) {
+              document.title = payload.title;
+              return;
+          }
+          if (payload.type === 'forum-ready') {
+              const msg = {
+                  type: 'forum-referrer',
+                  referrer: document.referrer,
+              };
+              sendMessage(msg);
+          }
+      });
+  })();
+
+}());
+//# sourceMappingURL=embed.js.map
