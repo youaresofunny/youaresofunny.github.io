1051914f760b311b12e231ea3573a95262915a3e
youaresofunny <anlopan@gmail.com>
2020-03-16 19:29:35 +0500

    Add prefix

--- embed.js
+++ embed.js
@@ -1,5 +1,6 @@
 (function () {
   'use strict';
+
   const env = {"BASE_URL":"https://dev.randomcoffee.us"};
 
   function isForumNavEvent(e) {
@@ -8,6 +9,7 @@
   function isForumTitleEvent(e) {
       return e.type === 'forum-title';
   }
+
   const unallowedInsertTags = ["P","SPAN","LI","UL","OL"];
   function insertSmart(what, where) {
     if (unallowedInsertTags.includes(where.parentElement.tagName)) {
@@ -18,32 +20,32 @@
   }
 
   (() => {
-      const DATA_ATTR = 'data-forum-id';
-      const thisScriptTag = document.querySelector(`script[${DATA_ATTR}]`);
+      const DATA_ATTR_ID = 'data-forum-id';
+      const DATA_ATTR_PREFIX = 'data-forum-prefix';
+      const thisScriptTag = document.querySelector(`script[${DATA_ATTR_ID}]`);
       if (!thisScriptTag)
           return; // should not be possible
-      const communityID = thisScriptTag.getAttribute(DATA_ATTR);
+      const communityID = thisScriptTag.getAttribute(DATA_ATTR_ID);
       if (!communityID) {
-          console.error(`${DATA_ATTR} must be defined`);
+          console.error(`${DATA_ATTR_ID} must be defined`);
           return;
       }
+      const prefix = thisScriptTag.getAttribute(DATA_ATTR_PREFIX) || '';
+      console.log('PREFIX', prefix);
       const baseURL = env.BASE_URL;
       const prefixRgx = new RegExp(`^/${communityID}`);
       const forumURL = new URL(baseURL);
       const { pathname, search } = window.location;
-      forumURL.pathname = `/${communityID}${pathname}`;
+      const strippedPath = prefix ? pathname.replace(`/${prefix}`, '') : pathname;
+      forumURL.pathname = `/${communityID}${strippedPath}`;
       forumURL.search = search;
       const iframe = document.createElement('iframe');
       iframe.src = forumURL.href;
       iframe.style.border = 'none';
       iframe.style.width = '100%';
       iframe.style.height = '100%';
-      iframe.id = 'circles-forum';
 
       insertSmart(iframe, thisScriptTag);
-
-      document.body.insertBefore(iframe, thisScriptTag);
-
 
       const sendMessage = (msg) => {
           const w = iframe.contentWindow;
@@ -62,7 +64,7 @@
           }
           const payload = event.data;
           if (isForumNavEvent(payload)) {
-              const location = payload.pathname.replace(prefixRgx, '') + payload.search;
+              const location = `${payload.pathname.replace(prefixRgx, prefix ? `/${prefix}` : '')}${payload.search}`;
               history.pushState(null, '', location);
               return;
           }
